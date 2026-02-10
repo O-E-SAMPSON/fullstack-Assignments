731dda335e02ee5b07df5c4d458118de2ccb6720
O-E-SAMPSON <sampsoneonyemaechi@gmail.com>
2026-02-10 21:56:46 +0100

    materials to work on assignments

---
+++ script.js
@@ -0,0 +1,19 @@
+//ASSIGNMENT
+
+const firstName = "Onye";
+console.log(firstName);
+
+const lastName = "Samp";
+console.log(lastName);
+
+const age = 75;
+console.log(age);
+
+const gender = "Male";
+console.log(gender);
+
+const phoneNumber = 803377;
+console.log(firstName);
+
+const email = "samman144@yahoo.com";
+console.log(email);
