a4e8b005fba98ee4fb712c4e56e97d49350e806d
O-E-SAMPSON <sampsoneonyemaechi@gmail.com>
2026-02-13 05:55:31 +0100

    assignment done

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 //ASSIGNMENT
 
-const firstName = "Onye";
-console.log(firstName);
+//const firstName = "Onye";
+//console.log(firstName);
 
-const lastName = "Samp";
-console.log(lastName);
+//const lastName = "Samp";
+//console.log(lastName);
 
-const age = 75;
-console.log(age);
+//const age = 75;
+//console.log(age);
 
-const gender = "Male";
-console.log(gender);
+//const gender = "Male";
+//console.log(gender);
 
-const phoneNumber = 803377;
-console.log(firstName);
+//const phoneNumber = 803377;
+//console.log(phoneNumber);
 
-const email = "samman144@yahoo.com";
-console.log(email);
+//const email = "samman144@yahoo.com";
+//console.log(email);
+
+// ASSIGNMENT
+// CREATE A PROGRAM THAT STORES A STUDENT SCORE AS A STRING
+
+let score = "69.8";
+console.log(typeof (score));
+console.log(typeof (parseInt(score)));
+//const score = number;
+//console.log(typeof (score.toFixed(score(0))));
+//console.log(typeof(decimal.toFixed(1)))
+
+if (score >= 70) {
+    console.log(`Your score is ${score} and your Grade is A`)
+} else if (score >= 60 && score <= 69) {
+    console.log(`Your score is ${score} and your Grade is B`)
+} else if (score >= 50 && score <= 59) {
+    console.log(`Your score is ${score} and your Grade is C`)
+} else if (score >= 40 && score <= 49) {
+    console.log(`Your score is ${score} and your Grade is D`)
+} else {
+    console.log(`Your score is ${score} and your Grade is F`)
+} 
